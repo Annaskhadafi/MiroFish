c4bcee07c238c296c4b3d5ce210ce73cb296af96
Annaskhadafi <mochamad.khadafi@chitraparatama.co.id>
2026-04-27 10:05:26 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,8 +12,10 @@ export default defineConfig({
     }
   },
   server: {
+    host: true,
     port: 3000,
-    open: true,
+    open: false,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
